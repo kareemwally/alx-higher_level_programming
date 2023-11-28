1a325747ab170dbb0a46365d9dcb9403c074c8bf
kareemwally <kareemwally@std.mans.edu.eg>
2023-11-28 12:36:59 -0800

    no builtins

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     res = ""
     for i in str:
-        if not (ord(i) > 64 and ord(i) < 91):
+        if (ord(i) > 96 and ord(i) < 123):
             res += chr(ord(i) - 32)
         else:
             res += i
