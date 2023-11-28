b384ff006f5b048a496557cdf03132dcffeda388
kareemwally <kareemwally@std.mans.edu.eg>
2023-11-28 11:47:16 -0800

    combinations

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+i = 1
+count=2
+while i < 89:
+    if not i%10:
+        i=i+count
+        count+=1
+    if i == 89:
+        break
+    print("{0:02d}".format(i), end=' ,')
+    i+=1
+print(i)
