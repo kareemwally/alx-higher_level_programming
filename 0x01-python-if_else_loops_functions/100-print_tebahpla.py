e5f30c252ced0a96be492ed7c58796f8b2473583
kareemwally <kareemwally@std.mans.edu.eg>
2023-11-28 14:19:37 -0800

    alternative

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(ord('z') , ord('a') - 1, -1):
+for i in range(ord('z'), ord('a') - 1, -1):
     if i % 2:
         print("{}".format(chr(i).upper()), end='')
     else:
-       print("{}".format(chr(i).lower()), end='')
-
+        print("{}".format(chr(i).lower()), end='')
