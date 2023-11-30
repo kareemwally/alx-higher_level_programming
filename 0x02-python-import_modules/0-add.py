30aa465ee7cdc98c705edcc13423f3b642d86127
kareemwally <kareemwally@std.mans.edu.eg>
2023-11-30 08:23:33 -0800

    aaaaaaa

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-import add_0
-a = 1
-b = 2
-print("{0} + {1} = {2}".format(a, b, add(a, b)))
+if "__main__" in dir():
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{0} + {1} = {2}".format(a, b, add(a, b)))
