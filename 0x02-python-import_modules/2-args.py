72e1b08016e5719f93383adc13312578e58af5c1
kareemwally <kareemwally@std.mans.edu.eg>
2023-11-30 12:29:00 -0800

    sys module again

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    n = len(sys.argv) - 1
+    print("{} arguments:".format(n) if n > 0 else "{} argument.".format(n))
+    for i in range(1, n + 1):
+        print("{} : {}".format(i, sys.argv[i]))
