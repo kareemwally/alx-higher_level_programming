4417013331b9b4e1b7e6d21323d6685af463c193
kareemwally <kareemwally@std.mans.edu.eg>
2023-12-02 14:43:56 -0800

    aaaaaaaah

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    two = sentence[0] if len(sentence) >= 0 else None
+    two = sentence[0] if len(sentence) > 0 else None
     one = len(sentence)
     return(one, two)
