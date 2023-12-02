55c0017052443ef0ddfac9a04d8f5005350d62f5
kareemwally <kareemwally@std.mans.edu.eg>
2023-12-02 14:21:30 -0800

    adding tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,8 +2,7 @@
 def add_tuple(tuple_a=(0, 0), tuple_b=(0, 0)):
     a = find_none(tuple_a)
     b = find_none(tuple_b)
-    res = (a[0] + b[0], a[1] + b[1])
-    return res
+    return(a[0] + b[0], a[1] + b[1])
 
 
 def find_none(a):
