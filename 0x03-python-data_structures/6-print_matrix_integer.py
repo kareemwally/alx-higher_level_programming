5cd52611f61bed9977fa2ad6657382cd20da787b
kareemwally <kareemwally@std.mans.edu.eg>
2023-12-02 10:03:49 -0800

    Comprehensions are so hard

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,8 @@ def print_matrix_integer(matrix=[[]]):
     if not matrix:
         return None
     for i in matrix:
-        if len(i):
-            for j in i[:-1]:
-                print("{:d}".format(j), end=' ')
-            print("{:d}".format(i[-1]))
+        if not len(i):
+            return print()
+        for j in i[:-1]:
+            print("{:d}".format(j), end=' ')
+        print("{:d}".format(i[-1]))
