6bc086b3c66d6b5f69d7269cf94f4c2cd69ac4b2
kareemwally <kareemwally@std.mans.edu.eg>
2023-12-02 15:11:30 -0800

    delete at fucking idx

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     res = my_list
-    if idx in range(0, len(my_list)):
+    if idx in range(len(my_list)):
         del(res[idx])
     return res
