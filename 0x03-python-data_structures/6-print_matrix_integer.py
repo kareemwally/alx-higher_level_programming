0010b50e78ec8be57d9799119d29b57438e6e570
kareemwally <kareemwally@std.mans.edu.eg>
2023-12-02 09:39:54 -0800

    int by int

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
+    if not matrix or len(matrix) == 0:
         return None
     for i in matrix:
-        res = ' '.join(str(element) for element in i)
-        print("{}".format(res))
+        for j in i[:-1]:
+            print("{:d}".format(j), end=' ')
+        print("{}".format(i[-1]))
