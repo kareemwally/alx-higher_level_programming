b7e5ae8ed5ce5300c5ddd9a6d45ec3dee1ad76ea
kareemwally <kareemwally@std.mans.edu.eg>
2023-12-02 15:05:56 -0800

    del()

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
+    res = my_list
     if idx in range(0, len(my_list)):
-        del(my_list[idx])
-    return my_list
+        del(res[idx])
+    return res
