b961f3029e927072bc60c1491bec9236065dd2e6
kareemwally <kareemwally@std.mans.edu.eg>
2023-12-03 04:21:21 -0800

    another solution

---
+++ 0x03-python-data_structures/alt_7.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(0, 0), tuple_b=(0, 0)):
+    a = find_none(tuple_a)
+    b = find_none(tuple_b)
+    return(a[0] + b[0], a[1] + b[1])
+
+
+def find_none(a):
+    res = list(a)
+    i = 2 - len(res)
+    for j in range(i):
+        res.append(0)
+    return(res)
