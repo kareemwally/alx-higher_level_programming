522240687ec1efbc1d9c36a9fd5e68618f211942
kareemwally <kareemwally@std.mans.edu.eg>
2023-12-03 04:32:14 -0800

    return tuple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    two = sentence[0] if len(sentence) > 0 else None
-    one = len(sentence)
-    return(one, two)
+    return(len(sentence), sentence[0] if len(sentence) > 0 else None)
