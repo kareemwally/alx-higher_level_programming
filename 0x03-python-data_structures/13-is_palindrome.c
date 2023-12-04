d7aaac6b0428b66d06ce2708833d51703836fc0f
kareemwally <kareemwally@std.mans.edu.eg>
2023-12-04 14:42:55 -0800

    IT' WROKING the checker'll kill me

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,7 +1,56 @@
 #include "lists.h"
 #include <stddef.h>
 /**
+ *check_even -check the list if it's even
+ *Description: simple function
+ *@mov:pointer to 1st node
+ *@current:pointer to last node
+ *Return: (int)
+ */
+int check_even(listint_t *mov, listint_t *current)
+{
+while(1)
+{
+if (mov->n != current->n)
+{
+return (0);
+}
+mov = mov ->next;
+if (mov == current)
+{
+break;
+}
+current = current - 2;
+}
+return (1);
+}
+/**
+ *check_odd -function to check odd_list nodes
+ *Description: simples function
+ *@mov:pointer to 1st node
+ *@current:pointer to last node
+ *Return: (int)
+ */
+int check_odd(listint_t *mov, listint_t *current)
+{
+while(1)
+{
+if (mov->n != current->n)
+{
+return (0);
+}
+if (mov == current)
+{
+break;
+}
+mov = mov ->next;
+current = current - 2;
+}
+return (1);
+}
+/**
  *is_palindrome :function to check symmetry
+ *Description: that function to check simitric
  *@head: pointer to the base pointer of list
  *Return: (int)
  */
@@ -22,35 +71,11 @@ n++;
 }
 if (n % 2 == 0)
 {
-while(1)
-{
-if (mov->n != current->n)
-{
-return (0);
-}
-mov = mov ->next;
-if (mov == current)
-{
-break;
-}
-current = current - 2;
-}
+return (check_even(mov, current));
 }
 else
 {
-while(1)
-{
-if (mov->n != current->n)
-{
-return (0);
-}
-if (mov == current)
-{
-break;
-}
-mov = mov ->next;
-current = current - 2;
-}
+return (check_odd(mov, current));
 }
 return (1);
 }
