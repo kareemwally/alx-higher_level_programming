c8d417a9cd6914716da12429899fb74ca1e42eb7
kareemwally <kareemwally@std.mans.edu.eg>
2023-12-04 09:21:13 -0800

    back to c

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,34 @@
+/**
+ *is_palindrome :function to check symmetry
+ *Description: that function checks symmetrical lists
+ *@head: pointer to the base pointer of list
+ *Return: (int)
+ */
+int is_palindrome(listint_t **head)
+{
+if (*head == NULL)
+{
+return (1);
+}
+listint_t *current, *mov;
+current = *head;
+mov = *head;
+while (current->next != NULL)
+{
+current = current->next;
+}
+while (1)
+{
+if (mov->n != current->n)
+{
+return (0);
+}
+mov = mov->next;
+if (mov == current)
+{
+break;
+}
+current = current - 2;
+}
+return (1);
+}
