bb2fdd2a93665b3e7e8bc986e153182c9d23658d
kareemwally <kareemwally@std.mans.edu.eg>
2023-12-06 11:08:42 -0800

    sorting keys

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    sorte = list(a_dictionary).sort()
+    sorte = list(a_dictionary)
+    sorte.sort()
     for i in sorte:
         print("{:s}: {}".format(i, a_dictionary[i]))
