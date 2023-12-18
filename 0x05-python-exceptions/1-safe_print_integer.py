66dd9a7bebe448ea0e60a82d243ab90a842e2986
kareemwally <kareemwally@std.mans.edu.eg>
2023-12-18 04:20:58 -0800

    printing integers safely

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if  int(value) == value:
+        if int(value) == value:
             print("{:d}".format(value))
             return True
         else:
