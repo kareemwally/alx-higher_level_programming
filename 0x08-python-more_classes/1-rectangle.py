78adf3f8194ea5373cdea77f6ac81cf098c5c17c
kareemwally <kareemwally@std.mans.edu.eg>
2024-01-06 17:55:37 -0800

    property

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,20 +8,21 @@ the rectangular class
 class Rectangle:
     """ adding some attributes"""
     def __init__(self, width=0, height=0):
-        Rectangle.width(self, width)
-        Rectangle.height(self, height)
+        self.width(self, width)
+        self.height(self, height)
 
+    @property
     def width(self):
         return self.__width
-
+    @property
     def height(self):
         return self.__height
-    
+    @width.setter
     def width(self, value):
         self.__width = value
         assert value >= 0, "width must be >= 0"
         assert type(value) == type(100), "width must be an integer"
-
+    @height.setter
     def height(self, value):
         self.__height = value
         assert value >= 0, "height must be >= 0"
