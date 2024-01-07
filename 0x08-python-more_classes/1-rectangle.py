90487ee0e3b0320143c3c9bd640da788ea8be902
kareemwally <kareemwally@std.mans.edu.eg>
2024-01-06 18:27:26 -0800

    properties

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,26 +4,37 @@ the rectangular class
 """
 
 
-
 class Rectangle:
     """ adding some attributes"""
     def __init__(self, width=0, height=0):
-        self.width(self, width)
-        self.height(self, height)
+        """initiation of object"""
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        return self.__width
+        """Width getting property"""
+        return self.width
+
     @property
     def height(self):
-        return self.__height
+        """Height getting property""" 
+        return self.height
+
     @width.setter
     def width(self, value):
-        self.__width = value
-        assert value >= 0, "width must be >= 0"
-        assert type(value) == type(100), "width must be an integer"
+        """setting width with conditions"""
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        self.width = value
+
     @height.setter
     def height(self, value):
-        self.__height = value
-        assert value >= 0, "height must be >= 0"
-        assert type(value) == type(100), "height must be an integer"
+        """setting height with conditions"""
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        self.height = value
