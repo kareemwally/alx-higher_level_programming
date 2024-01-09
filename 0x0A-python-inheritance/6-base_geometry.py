830bd8b0b86c75145a60d32f9ef38a1870ad96c2
kareemwally <kareemwally@std.mans.edu.eg>
2024-01-08 16:44:04 -0800

    raising exceptions

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+improving the BaseGeometry class"""
+
+
+class BaseGeometry:
+    """the basegeomtry improvment"""
+    def area(self):
+        raise Exception("area() is not implemented")
