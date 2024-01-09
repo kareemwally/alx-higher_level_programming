54f43f4cd3bed032e0d53235096fbbf9ca1257f7
kareemwally <kareemwally@std.mans.edu.eg>
2024-01-08 16:29:52 -0800

    is sub class?

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+function to check the base or inherited class
+"""
+
+
+def inherits_from(obj, a_class):
+    """using issubclass method"""
+    return issubclass(obj, a_class)
