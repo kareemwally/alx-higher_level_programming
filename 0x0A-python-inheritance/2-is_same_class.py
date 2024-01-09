28c31ca7aa9dbde0c8b4d3912912d322818cce5a
kareemwally <kareemwally@std.mans.edu.eg>
2024-01-08 16:14:11 -0800

    is instance not working ????

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,7 +5,5 @@ function to test instance based class
 
 
 def is_same_class(obj, a_class):
-    """
-    simple functio using isinstance method
-    """
-    return isinstance(obj, a_class)
+    """simple functio using isinstance method"""
+    return type(obj) ==  a_class
