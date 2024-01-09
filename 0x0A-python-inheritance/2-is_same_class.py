a7a57f692069201e1316b6806dd06b2161741266
kareemwally <kareemwally@std.mans.edu.eg>
2024-01-08 16:03:54 -0800

    is instance?

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+function to test instance based class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    simple functio using isinstance method
+    """
+    return isinstance(obj, a_class)
