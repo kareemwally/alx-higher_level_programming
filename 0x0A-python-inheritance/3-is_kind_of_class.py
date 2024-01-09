8b611994201b1c2d35cfb915e13b69fd9434fe91
kareemwally <kareemwally@std.mans.edu.eg>
2024-01-08 16:23:32 -0800

    is instance?

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+function to check instance base class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """checking the object base class"""
+    return isinstance(obj, a_class)
