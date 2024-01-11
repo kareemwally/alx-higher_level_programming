63b616df42b5986cd1ec27dc9479fb5cf8da27df
karem wally <karemwally@std.mans.edu.eg>
2024-01-11 00:58:36 +0000

    just reading

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,6 @@ simple functio to print the content of file
 
 
 def read_file(filename=""):
-	"""using with statement"""
-	with open(filename, 'r', encoding =  'UTF8') as M:
-		print(M.read())
+    """using with statement"""
+    with open(filename, 'r', encoding='UTF8') as M:
+        print(M.read())
