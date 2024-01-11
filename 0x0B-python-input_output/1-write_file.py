a8c3054e4082bcad486a1809342825d237f352a1
karem wally <karemwally@std.mans.edu.eg>
2024-01-11 01:29:55 +0000

    just writting

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,5 +7,5 @@ a simple function to write a sentence into a file
 def write_file(filename="", text=""):
     """using with statement"""
     with open(filename, 'w', encoding='utf-8') as W:
-        print("{}".format(text), file=W)
+        print("{}".format(text), end='', file=W)
     return len(text)
