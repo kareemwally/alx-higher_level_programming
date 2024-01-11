5e1865341f8fcc9564ddbdcd850a6f720ec2ddac
karem wally <karemwally@std.mans.edu.eg>
2024-01-11 01:21:27 +0000

    just writting

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+a simple function to write a sentence into a file
+"""
+
+
+def write_file(filename="", text=""):
+    """using with statement"""
+    with open(filename, 'w', encoding='utf-8') as W:
+        print("{}".format(text), file=W)
+    return len(text)
