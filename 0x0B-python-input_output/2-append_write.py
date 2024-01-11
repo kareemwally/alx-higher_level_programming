6a1a1f987a3fdb30ba328ac813c4d543a474b6b6
karem wally <karemwally@std.mans.edu.eg>
2024-01-11 01:40:12 +0000

    appending

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+a simple function to append sentence to a file
+"""
+
+
+def append_write(filename="", text=""):
+    with open(filename, 'a', encoding='utf-8') as A:
+        print("{}".format(text), end="", file=A)
+    return len(text)
