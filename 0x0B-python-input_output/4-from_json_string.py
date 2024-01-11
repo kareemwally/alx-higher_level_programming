ef45c1e2a84402215edfe908979fc1a3915087c0
karem wally <karemwally@std.mans.edu.eg>
2024-01-11 02:34:24 +0000

    loads method

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+a simple function to invert Json into python datatype
+"""
+
+import json
+
+
+def from_json_string(my_str):
+    """the json.loads method"""
+    res = json.loads(my_list)
+    return res
