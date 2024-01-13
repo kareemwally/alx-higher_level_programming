0e24b4d4573df843da0f04402b3502b96451459b
karem wally <karemwally@std.mans.edu.eg>
2024-01-13 07:02:39 +0000

    I'm tires, Alfred

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,11 +5,15 @@ that function only add two and only two integer type data
 >>> add_integer('a', 22)
 Traceback (most recent call last):
         ...
-    TypeError: a must be an integer
+TypeError: a must be an integer
 >>> add_integer(2, 'a')
 Traceback (most recent call last):
         ...
-    TypeError: b must be an integer
+TypeError: b must be an integer
+>>> add_integer(float('NaN'))
+Traceback (most recent call last):
+        ...
+ValueError: cannot convert float NaN to integer
 """
 
 
