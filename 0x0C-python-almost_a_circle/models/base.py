2cb25cf548bf8efd1d42614d9afc6c7d5618e33c
karem wally <karemwally@std.mans.edu.eg>
2024-01-16 02:00:23 +0000

    the base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+base class
+"""
+
+
+class Base:
+    """body of base class"""
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """intializing instance"""
+        if id not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
