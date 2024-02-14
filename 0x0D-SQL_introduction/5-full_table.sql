e62a74ac97829962bc2624cd8a6f54406450b311
kareemwally <kareemwally@std.mans.edu.eg>
2024-02-13 17:10:49 -0800

    show description

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,6 +1,3 @@
 -- that script to get description of table
 -- without using `DESCRIPTION` command
-SELECT column_name, data_type, character_maximum_length, is_nullable
-FROM information_schema.columns
-WHERE table_schema = 'hbtn_0c_0 '
-AND table_name = 'first_table';
+SHOW CREATE TABLE first_table;
