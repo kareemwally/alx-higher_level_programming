f7ed73c37ef3dbbb9bfb9ae8b6a225ef1d5b158b
kareemwally <kareemwally@std.mans.edu.eg>
2024-02-13 17:05:59 -0800

    character set

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -4,7 +4,7 @@ DEFAULT COLLATE = utf8mb4_unicode_ci;
 -- ALTER THE TABLE character set ITSELF
 USE hbtn_0c_0;
 ALTER TABLE first_table
-CONVERT TO CHARACTER SET utf8mb4;
+CONVERT TO CHARACTER SET utf8mb4 COLLATE  utf8mb4_unicode_ci;
 -- alter a column
 USE hbtn_0c_0;
 ALTER TABLE first_table
