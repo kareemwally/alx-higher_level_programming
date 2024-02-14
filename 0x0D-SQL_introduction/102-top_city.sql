0c0ae2958f78a07cee9754747b1b40ed495b2f72
kareemwally <kareemwally@std.mans.edu.eg>
2024-02-14 05:13:05 -0800

    th top 3 averages

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,7 @@
 -- that script is get the max 3
 SELECT city, AVG(value) AS avg_temp
 FROM temperatures
-WHERE month = 'August' OR month = 'July'
+WHERE month = 7 OR month = 8
 GROUP BY city
 ORDER BY AVG(value) DESC
 LIMIT 3;
