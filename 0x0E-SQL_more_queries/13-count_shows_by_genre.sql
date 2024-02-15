be897fa99ed8f29dd2686dae536eb85ee135aab3
kareemwally <kareemwally@std.mans.edu.eg>
2024-02-15 09:18:05 -0800

    the tv_shows

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- script to link genre to SUM of works related to it
 USE hbtn_0d_tvshows;
 SELECT tv_genres.name,
-COUNT(tv_genres.id)
+COUNT(tv_genres.id) AS number_of_shows
 FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
