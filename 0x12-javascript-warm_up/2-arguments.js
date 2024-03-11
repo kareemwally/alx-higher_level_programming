6fa2d78d12ddaf3d90b83a97b6d03d5567e6005b
= <kareemwally@std.mans.edu.eg>
2024-03-11 19:10:03 +0000

    cmd arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+let x = process.argv
+if(x.length < 3)
+{
+console.log('No argument');
+}
+else if (x.length == 3)
+{
+console.log('Argument found')
+}
+else
+{
+console.log('Arguments found')
+}
