76d3b5be6e75690cfc8ce315debbbe1675d25f62
= <kareemwally@std.mans.edu.eg>
2024-03-12 23:22:04 +0000

    rectangle dimensions

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (h > 0 && w > 0) {
+      this.height = h;
+      this.width = w;
+    } else {
+      Object.create(null);
+    }
+  }
+
+  print () {
+    let row = '';
+    for (let i = 0; i < this.width; i++) {
+      row += 'X';
+    }
+    for (let j = 0; j < this.height; j++) {
+      console.log(row);
+    }
+  }
+}
+module.exports = Rectangle;
