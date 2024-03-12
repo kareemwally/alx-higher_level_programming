a073e603c2a92d710ef823752f6e419b26988aa2
= <kareemwally@std.mans.edu.eg>
2024-03-12 21:46:49 +0000

    test cases

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    this.height = h > 0 && w > 0 ? h : undefined;
-    this.width = w > 0 && h > 0 ? w : undefined;
+    if ( h > 0 && w > 0) {
+      this.height = h;
+      this.width = w;
+    }
   }
 }
 module.exports = Rectangle;
