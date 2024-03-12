90dcbae61bdceceb9a044f10a11de4e48dac6f28
= <kareemwally@std.mans.edu.eg>
2024-03-12 00:51:37 +0000

    integer parser

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
-let x = process.argv;
-let y = parseInt(x[2]);
-if (isNaN(y))
-{
-console.log('Not a number');
-}
-else
-{
-console.log('My number: ' + y);
+const x = process.argv;
+const y = parseInt(x[2]);
+if (isNaN(y)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + y);
 }
