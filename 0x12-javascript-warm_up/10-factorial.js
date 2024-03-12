fd75e3af28d1b939cd9962bcbc8796d98bb31a45
= <kareemwally@std.mans.edu.eg>
2024-03-12 00:47:02 +0000

    recursion factoria;

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const x = parseInt(process.argv[2]);
 function fac (a) {
-  if (isNaN(a) || a == 0) {
+  if (isNaN(a) || a === 0) {
     return (1);
-}
-return a * fac(a - 1);
+  }
+  return a * fac(a - 1);
 }
 console.log(fac(x));
