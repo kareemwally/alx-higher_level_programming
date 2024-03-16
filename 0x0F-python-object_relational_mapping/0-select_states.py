1e4b361001eae5f9c5dca4ea697757a4dd1012bd
= <kareemwally@std.mans.edu.eg>
2024-03-16 04:35:37 +0000

    disguised SQL

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+cur = db.cursor()
+cur.execute("USE hbtn_0e_0_usa;")
+cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+db.close()
