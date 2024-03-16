811005956e182cc5531aa1050d9e0bfcf844d6e6
= <kareemwally@std.mans.edu.eg>
2024-03-16 18:49:48 +0000

    the set

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" a module to connect to a localhost db and search for a state name """
+import MySQLdb
+import sys
+
+
+def main():
+    """ the main function to execute SQL query """
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+        )
+    cur = db.cursor()
+    cur.execute(
+        "SELECT cities.name FROM cities\
+        INNER JOIN states ON cities.state_id = states.id\
+        WHERE states.name = '{}'\
+        ORDER BY cities.id ASC;".format(sys.argv[4]))
+    res = cur.fetchall()
+    final = set(i[0] for i in res)
+    print(*final, sep=", ")
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
