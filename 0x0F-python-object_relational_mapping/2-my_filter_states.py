48b7e00dbc5a3f561416420814801d256a0472ba
= <kareemwally@std.mans.edu.eg>
2024-03-16 14:39:21 +0000

    name

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" a module to connect to a localhost db and search for a state name """
+import MySQLdb
+import sys
+
+
+def main():
+    """ the main function to execute SQL query """
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+        )
+    cur = db.cursor()
+    query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC;"
+    cur.execute(query.format(sys.argv[4]))
+    res = cur.fetchall()
+    for i in res:
+        print(i)
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
