a116fe58cb63a9ba3282c3724107d4fc941c4381
= <kareemwally@std.mans.edu.eg>
2024-03-16 12:38:01 +0000

    SQL x py script

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
+''' a python script to connect ti local host db'''
 import MySQLdb
 import sys
-db = MySQLdb.connect(
-    host="localhost", user=sys.argv[1],
-    passwd=sys.argv[2], db=sys.argv[3]
-    )
-cur = db.cursor()
-cur.execute("USE {0};".format(sys.argv[3]))
-cur.execute(
-    "SELECT * FROM {0} ORDER BY {1} ASC LIMIT 5;"
-    .format('states', 'states.id')
-    )
-res = cur.fetchall()
-for i in res:
-    print(i)
-cur.close()
-db.close()
+def main():
+    ''' the main function'''
+    db = MySQLdb.connect(
+        host="localhost", user=sys.argv[1],
+        passwd=sys.argv[2], db=sys.argv[3]
+        )
+    cur = db.cursor()
+    cur.execute("USE {0};".format(sys.argv[3]))
+    cur.execute(
+        "SELECT * FROM {0} ORDER BY {1} ASC LIMIT 5;"
+        .format('states', 'states.id')
+        )
+    res = cur.fetchall()
+    for i in res:
+        print(i)
+    cur.close()
+    db.close()
+if __name__ == "__main__":
+    main()
