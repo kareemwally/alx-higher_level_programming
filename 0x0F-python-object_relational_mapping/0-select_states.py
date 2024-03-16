4f816191fb0f01249d0232a4199faad0d9f6566f
= <kareemwally@std.mans.edu.eg>
2024-03-16 05:02:39 +0000

    hidden SQL

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
 import MySQLdb
 import sys
-db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+db = MySQLdb.connect(
+    host="localhost", user=sys.argv[1],
+    passwd=sys.argv[2], db=sys.argv[3]
+    )
 cur = db.cursor()
-cur.execute("USE hbtn_0e_0_usa;")
-cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+cur.execute("USE {0};".format(sys.argv[3]))
+cur.execute(
+    "SELECT * FROM {0} ORDER BY {1} ASC;"
+    .format('states', 'states.id')
+    )
+res = cur.fetchall()
+for i in res:
+    print(i)
+cur.close()
 db.close()
