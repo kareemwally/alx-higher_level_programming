d9a843e4d8d4620e474edcf3cca21dd66c6434d9
= <kareemwally@std.mans.edu.eg>
2024-03-17 17:31:04 +0000

    fetching

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""a module to list all states """
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import sys
+from model_state import Base, State
+connect = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+engine = create_engine(connect.format(sys.argv[1], sys.argv[2], sys.argv[3]))
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+query = session.query(State).order_by(State.id).all()
+session.commit()
+
+for s in query:
+    print("{}: {}".format(s.id, s.name))
+
+session.close()
