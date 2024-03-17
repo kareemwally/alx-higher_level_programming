19cf158a5fbe4dd5224af5c247d4adaaed999cab
= <kareemwally@std.mans.edu.eg>
2024-03-17 18:09:17 +0000

    first

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""a module to list all states """
+
+
+def main():
+    """ the main function"""
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy.ext.declarative import declarative_base
+    from sqlalchemy import create_engine
+    import sys
+    from model_state import Base, State
+    connect = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+    engine = create_engine(
+        connect.format(sys.argv[1], sys.argv[2], sys.argv[3])
+        )
+    Session = sessionmaker(bind=engine)
+
+    Base.metadata.create_all(engine)
+
+    session = Session()
+    query = session.query(State).first()
+
+    if query:
+        print("{}: {}".format(query.id, query.name))
+    else:
+        print("Nothing")
+
+    session.close()
+
+
+if __name__ == "__main__":
+    main()
