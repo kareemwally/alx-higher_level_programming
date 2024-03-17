65d46c93a440987a394843efba78d872813cbe62
= <kareemwally@std.mans.edu.eg>
2024-03-17 17:51:54 +0000

    fetching states

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,21 +1,31 @@
 #!/usr/bin/python3
 """a module to list all states """
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
-import sys
-from model_state import Base, State
-connect = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
-engine = create_engine(connect.format(sys.argv[1], sys.argv[2], sys.argv[3]))
-Session = sessionmaker(bind=engine)
 
-Base.metadata.create_all(engine)
 
-session = Session()
-query = session.query(State).order_by(State.id).all()
-session.commit()
+def main():
+    """ the main function"""
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy.ext.declarative import declarative_base
+    from sqlalchemy import create_engine
+    import sys
+    from model_state import Base, State
+    connect = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+    engine = create_engine(
+        connect.format(sys.argv[1], sys.argv[2], sys.argv[3])
+        )
+    Session = sessionmaker(bind=engine)
 
-for s in query:
-    print("{}: {}".format(s.id, s.name))
+    Base.metadata.create_all(engine)
 
-session.close()
+    session = Session()
+    query = session.query(State).order_by(State.id).all()
+    session.commit()
+
+    for s in query:
+        print("{}: {}".format(s.id, s.name))
+
+    session.close()
+
+
+if __name__ == "__main__":
+    main()
