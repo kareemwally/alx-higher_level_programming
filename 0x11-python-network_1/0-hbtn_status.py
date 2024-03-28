d7b007d53390df3f2d09c86e580a36e12aad05a8
= <kareemwally@std.mans.edu.eg>
2024-03-28 15:52:05 +0000

    http request status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
 """ a simple script to fetch some URL"""
 import urllib.request
-with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as res:
-    content = res.read()
-    print("Body response:")
-    print("    - type: {0}".format(type(content)))
-    print("    - content: {0}".format(content))
-    print("    - utf8 content: {0}".format(content.decode('utf-8')))
+
+
+def main():
+    """ a script to fetch status of html request"""
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as res:
+        content = res.read()
+        print("Body response:")
+        print("    - type: {0}".format(type(content)))
+        print("    - content: {0}".format(content))
+        print("    - utf8 content: {0}".format(content.decode('utf-8')))
+
+
+if __name__ == "__main__":
+    main()
