a66dc2dbf946aa2b288c77eca421012ca9c99695
= <kareemwally@std.mans.edu.eg>
2024-03-28 15:29:18 +0000

    status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,5 +2,8 @@
 """ a simple script to fetch some URL"""
 import urllib.request
 with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as res:
-    html = res.read()
-    print(html)
+    content = res.read()
+    print("Body response:")
+    print(f"	- type:{type(content)}")
+    print(f"	-content:{content}")
+    print(f"	-utf8 content:{content.encode('utf-8')}")
