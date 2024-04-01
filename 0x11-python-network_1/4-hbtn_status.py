890e57d9c4087544209f3b233135c2ea26555aa8
= <kareemwally@std.mans.edu.eg>
2024-04-01 06:43:44 +0000

    first requests script

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""fetching URLs using Requests module"""
+import requests
+
+
+def Request_fetch():
+    """ a function to fetch urls"""
+    req = requests.get('https://alx-intranet.hbtn.io/status')
+    print('Body response:')
+    print('	- type: {0}'.format(type(req)))
+    print('	- content: {0}'.format(req.content.decode('utf-8')))
+
+
+if __name__ == "__main__":
+    Request_fetch()
