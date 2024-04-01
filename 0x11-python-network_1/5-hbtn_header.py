f4884745516dff95fa2fc38a80ac6af9498fa152
= <kareemwally@std.mans.edu.eg>
2024-04-01 07:13:02 +0000

    getting x_id

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
-""" a python script to get a specific data"""
+"""Fetching a specific data from a URL using the requests package"""
 import requests
 import sys
 
 
 def main():
-    """ getting a specific params"""
-    res = requests.get(sys.argv[1])
-    res_dict = res.json()
-    print(res_dict["X-Request-Id"])
+    """Get the value of X-Request-Id from the response header"""
+    try:
+        url = sys.argv[1]
+        response = requests.get(url)
+        x_request_id = response.headers.get("X-Request-Id")
+        if x_request_id:
+            print("X-Request-Id: {0}".format(x_request_id))
+        else:
+            print("X-Request-Id not found in the response headers.")
+    except IndexError:
+        print("Usage: python script.py <URL>")
 
 
 if __name__ == "__main__":
