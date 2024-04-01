aefecbf8e1fc528bf6dc061e67481e87400ca762
= <kareemwally@std.mans.edu.eg>
2024-04-01 01:53:01 +0000

    posting

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,17 +1,30 @@
 #!/usr/bin/python3
-""" a simple script to fetch some URL"""
-import urllib.request
 import sys
+import urllib.parse
+import urllib.request
 
 
-def main():
-    """ a script to fetch status of html request"""
-    email = sys.argv[2]
-    data = urlib.parse.urlencode({"email": email}).encode("utf-8")
-    with urllib.request.urlopen(sys.argv[1], data=data) as res:
-        content = res.read().decode("utf-8")
-        print("Your email is: {0}".format(content))
+def send_post_request(url, email):
+    try:
+        # Encode the email parameter
+        data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+        req = urllib.request.Request(url, data, method='POST')  # Specify method='POST'
+        # Create a POST request
+        with urllib.request.urlopen(req) as response:
+            # Read the response body and decode it
+            response_body = response.read().decode('utf-8')
+            print(response_body)  # Print the response body
+    except urllib.error.HTTPError as e:
+        print("HTTP Error: {0}, {1}".format(e.code, e.reason))
+    except urllib.error.URLError as e:
+        print("URL Error: {0}".format(e.reason))
 
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) != 3:
+        print("Usage: python script.py <URL> <email>")
+        sys.exit(1)
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+    send_post_request(url, email)
