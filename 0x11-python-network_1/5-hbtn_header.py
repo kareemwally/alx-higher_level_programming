acc84bbad52994481f0acb424c239d37f37b9228
= <kareemwally@std.mans.edu.eg>
2024-04-01 07:16:39 +0000

    getting x_id

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -11,7 +11,7 @@ def main():
         response = requests.get(url)
         x_request_id = response.headers.get("X-Request-Id")
         if x_request_id:
-            print("X-Request-Id: {0}".format(x_request_id))
+            print("{0}".format(x_request_id))
         else:
             print("X-Request-Id not found in the response headers.")
     except IndexError:
