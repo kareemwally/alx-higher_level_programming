a68a41c36881f1c9ee9e2a5a7cd633b816032c2f
kareem <kareemwally@std.mans.edu.eg>
2024-07-30 13:52:38 +0300

    writting to file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const filename = process.argv[2];
+const data = process.argv[3];
+const encoding = 'utf-8';
+fs.writeFile(filename, data, encoding, function (err) {
+  if (err) {
+    console.log(err);
+  }
+});
