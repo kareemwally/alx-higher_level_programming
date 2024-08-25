69a06e4f1cd188c3afb239206ccee88a5e267d09
kareem <kareemwally@std.mans.edu.eg>
2024-08-26 00:11:30 +0300

    only integers

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,7 +5,7 @@ def safe_print_integer_err(value):
     """simple function to print integrs"""
     try:
         print("{:d}".format(value))
-    except ValueError as e:
+    except (TypeError, ValueError) as e:
         print(e)
         return False
     return True
