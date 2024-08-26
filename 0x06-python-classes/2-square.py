582c720782fec3cc4551f9a2bc21157923162b6a
kareem <kareemwally@std.mans.edu.eg>
2024-08-26 14:16:36 +0300

    adding precautions

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,7 +7,7 @@ adding some attributes and features to the class
 class Square:
     """adding size instance attribute"""
     def __init__(self, size=0):
-        if size is not int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
