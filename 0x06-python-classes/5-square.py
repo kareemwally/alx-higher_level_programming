09b4ddaf7bea9ba147e5f66737b5463dc1c5d01f
kareem <kareemwally@std.mans.edu.eg>
2024-08-26 18:39:53 +0300

    print in #

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+adding some attributes and features to the class
+"""
+
+
+class Square:
+    """adding size instance attribute"""
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        the getter method of size property
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        setting the property size with some precautions
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        the area of square is size^2
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """
+        printing the shape of square with # char
+        """
+        for i in range(self.size):
+            print('#'*self.size)
