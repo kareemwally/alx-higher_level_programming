3b4d13e663f1b99addac9f2fb177fd441ca288c6
kareem <kareemwally@std.mans.edu.eg>
2024-09-28 23:40:42 +0300

    testing

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -3,6 +3,13 @@
 a simple function to divide a 2D array `matrix` by a number `div`
 """
 
+Errors_msgs = [
+        "div must be a number",
+        "division by zero",
+        "Each row of the matrix must have the same size",
+        "matrix must be a matrix(list of lists) of integers/floats"
+        ]
+
 
 def matrix_divided(matrix, div):
     """
@@ -10,20 +17,19 @@ def matrix_divided(matrix, div):
     and div must be a real positive number
     """
     if type(div) not in [int, float]:
-        raise TypeError("div must be a number")
+        raise TypeError(Errors_msgs[0])
 
     if div == 0:
-        raise ZeroDivisionError("division by zero")
+        raise ZeroDivisionError(Errors_msgs[1])
 
     size = len(matrix[0])
     res = []
     for array in matrix:
         if size != len(array):
-            raise TypeError("Each row of the matrix must have the same size")
+            raise TypeError(Errors_msgs[2])
         for ele in array:
             if type(ele) not in [float, int]:
-                raise TypeError("matrix must be a matrix(list of lists)\
-                        of integers/floats")
+                raise TypeError(Errors_msgs[3])
         res.append([round(ele/div, 2) for ele in array])
 
     return res
