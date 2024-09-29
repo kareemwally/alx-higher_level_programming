dde0caaedaca75068220d19e113bb42189501719
kareem <kareemwally@std.mans.edu.eg>
2024-09-29 07:46:04 +0300

    string manupliation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+a simple function to print a seprated text on one of these
+chars (`?`,`:`,`.`)
+"""
+
+
+def least_index(res, start=0):
+    """
+    a simple function to get the 1st index of one (`?`,`.`,`:`)
+    """
+    try:
+        dot = res.find('.', start)
+        ques = res.find('?', start)
+        colon = res.find(':', start)
+    except IndexError:
+        return None
+    else:
+        if dot < 0:
+            return ques if ques < colon or colon == -1 else colon
+        if ques < 0:
+            return dot if dot < colon or colon == -1 else colon
+        if colon < 0:
+            return dot if dot < ques or ques == -1 else ques
+        return dot if dot < ques and dot < colon \
+            else ques if ques < dot and ques < colon else colon
+    return -1
+
+
+def text_indentation(text):
+    """ the main function to seperate `text` base on
+    (`:`,`?`,`.`)
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    start = 0
+    while True:
+        end = least_index(text, start)
+        if end < 0:
+            print(text[start:].strip())
+            break
+        sub = text[start:end + 1]
+        print(sub.strip(), '\n')
+        start = end+1
